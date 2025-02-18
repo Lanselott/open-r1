3721eea6f6b4d94743b0bcdf4f4f74873fa64604
Lanselott <chenran1995@link.cuhk.edu.hk>
2025-02-18 17:02:16 +0800

    update experiments script

---
+++ eval_script.sh
@@ -0,0 +1,10 @@
+NUM_GPUS=4
+MODEL=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B
+MODEL_ARGS="pretrained=$MODEL,dtype=bfloat16,data_parallel_size=$NUM_GPUS,max_model_length=32768,gpu_memory_utilisation=0.8"
+TASK=aime24
+OUTPUT_DIR=data/evals/$MODEL
+
+lighteval vllm $MODEL_ARGS "custom|$TASK|0|0" \
+    --custom-tasks src/open_r1/evaluate.py \
+    --use-chat-template \
+    --output-dir $OUTPUT_DIR
---
+++ moe_sft_test.sh
@@ -0,0 +1,18 @@
+#accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft.py  --trust_remote_code True \
+#    --config recipes/DeepSeek-V2-Lite-Chat/sft/config_demo_moe.yaml
+
+#accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft.py  --trust_remote_code True \
+#    --config recipes/DeepSeek-V2-Lite-Chat/sft/config_moe_ot114k.yaml
+
+#accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft.py  --trust_remote_code True \
+#    --config recipes/DeepSeek-V2-Lite-Chat/sft/config_moe_numinamath.yaml
+
+#accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft_lora.py  --trust_remote_code True \
+#    --config recipes/DeepSeek-V2-Lite-Chat/sft/config_moe_lora_numinamath.yaml
+
+## single gpu test
+#accelerate launch --main_process_port 29499 --config_file recipes/accelerate_configs/zero3_singlegpu.yaml src/open_r1/sft.py  --trust_remote_code True \
+#    --config recipes/DeepSeek-V2-Lite-Chat/sft/config_moe_numinamath.yaml
+
+accelerate launch --config_file recipes/accelerate_configs/zero3.yaml src/open_r1/sft.py  --trust_remote_code True \
+    --config recipes/OLMoE-1B-7B-0125-Instruct/sft/config_bs_17k.yaml
